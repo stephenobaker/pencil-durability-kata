f72678c21c4e411bd438d3234868f7c79f6ac62a
Stephen Baker <stephengbaker@yahoo.com>
2019-09-24 20:22:15 -0500

    Passing: Pencil can be given point durability value when created

--- pencil.js
+++ pencil.js
@@ -1,4 +1,10 @@
 class Pencil {
+	constructor(durability) {
+		this.durabilityLeft = durability;
+	}
+	get durability() {
+		return this.durabilityLeft;
+	}
 	write(string, paper) {
 		paper.content = paper.content + string;
 	}
--- test/test.js
+++ test/test.js
@@ -6,7 +6,7 @@ import 'chai/register-expect';
 
 
 describe('a pencil', () => {
-	describe('it can write on a piece of paper', () => {
+	describe('can write on a piece of paper', () => {
   		let pencil = new Pencil();
     	let paper = new Paper();
 	    it('adds text to a blank piece of paper', () => {
@@ -17,5 +17,32 @@ describe('a pencil', () => {
 	    	pencil.write(' World!', paper);
 	    	expect(paper.content).to.equal('Hello World!');
 	    });
+	});
+	describe('has a point that can degrade', () => {
+		it('can be provided with a value for point durability', () => {
+			let pencil5 = new Pencil(5);
+			let pencil4000 = new Pencil(4000);
+			expect(pencil5.durability).to.equal(5);
+			expect(pencil4000.durability).to.equal(4000);
+		});
+		xit('writes blank spaces if it has degraded', () => {
+			let pencil = new Pencil(5);
+			let paper = new Paper();
+			pencil.write('helloworld', paper);
+			expect(paper.content).to.equal('hello     ');
+
+		});
+		xit('degrades by 0 for each space or newline written', () => {
+
+		});
+		xit('degrades by 1 for each lowercase letter written', () => {
+
+		});
+		xit('degrades by 2 for each uppercase letter written', () => {
+
+		});
+		xit('degrades by an unkown amount for each non-letter character written', () => {
+
+		});
 	});
 });
